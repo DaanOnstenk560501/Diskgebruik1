7088768f5037f4a49a9e31417a0a1a2bd7b478c1
Daan Onstenk 560501 <560501@student.fontys.nl>
2025-03-11 09:04:52 +0100

    Commit 2, folder pad is niet meer hardcoded. Eerst werden de bestanden die doorlopen werden weergegeven in de terminal nu gaat dit naar een log bestand. simpele foutafhandeling toegevoegd.

--- tempCodeRunnerFile.go
+++ tempCodeRunnerFile.go
@@ -3,53 +3,42 @@ package main
 import (
         "bufio"
         "fmt"
-        "log"
         "os"
         "path/filepath"
         "strings"
 )
 
 func main() {
-        logFile, err := os.OpenFile("folder_gebruik.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-        if err != nil {
-                log.Fatal("Fout bij het openen van logbestand:", err)
-        }
-        defer logFile.Close()
-
-        log.SetOutput(logFile)
-
         reader := bufio.NewReader(os.Stdin)
         fmt.Print("Voer het pad in: ")
         path, _ := reader.ReadString('\n')
         path = strings.TrimSpace(path)
 
-        log.Println("\nScan 1:") // Nieuwe scan markeren
-        totalSize := getDocumentsSize(path, logFile)
-        log.Println("Totale grootte van documentbestanden in", path, ":", formatBytes(totalSize))
+        totalSize := getDocumentsSize(path)
+        fmt.Printf("Totale grootte van documentbestanden in %s: %s\n", path, formatBytes(totalSize))
 
-        fmt.Println("Totale grootte van documentbestanden in", path, ":", formatBytes(totalSize))
         fmt.Println("\nDruk op Enter om af te sluiten...")
         bufio.NewReader(os.Stdin).ReadBytes('\n')
 }
 
-func getDocumentsSize(drive string, logFile *os.File) int64 {
+func getDocumentsSize(drive string) int64 {
         var totalSize int64
         err := filepath.Walk(drive, func(path string, info os.FileInfo, err error) error {
                 if err != nil {
-                        log.Println("Fout bij het bezoeken van:", path, err)
+                        fmt.Println("Fout bij het ophalen van:", path, err)
                         return err
                 }
                 if !info.IsDir() {
-                        log.Println("Bezocht bestand:", path, "extensie:", filepath.Ext(path))
+                        fmt.Println("Bestand gevonden:", path, "extensie:", filepath.Ext(path)) // Log bezochte bestanden
                         if isDocumentFile(path) {
-                                log.Println("Document bestand:", path)
+                                fmt.Println("Document bestand:", path)
                                 totalSize += info.Size()
                         }
                 }
                 return nil
         })
         if err != nil {
-                log.Println("Fout bij het doorlopen van de schijf:", err)
+                fmt.Println("Fout bij het ophalen van de schijf:", err)
         }
         return totalSize
 }
